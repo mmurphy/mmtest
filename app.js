a7ddc20493e69e18629592ea8eb848e99ad85254
Martin Murphy <martin.murphy@feedhenry.com>
2017-11-17 12:39:50 +0000

    Add endpoint to fail readiness probe, and return hostname with norma message

--- app.js
+++ app.js
@@ -2,10 +2,29 @@ const express = require('express');
 const app = express();
 
 const port = 4000;
+var shouldFail = false;
 
-app.get('/sys/ready', (req, res) => res.json({
-  'status': 'ok'
-}));
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/sys/ready', (req, res) => {
+  if (shouldFail) {
+    res.sendStatus(500);
+  } else {
+    res.json({
+      'status': 'ok'
+    });
+  }
+});
+app.get('/sys/fail', (req, res) => {
+  shouldFail = true;
+  res.json({
+    'status': 'ok'
+  })
+});
+app.get('/sys/fix', (req, res) => {
+  shouldFail = false;
+  res.json({
+    'status': 'ok'
+  })
+});
+app.get('/', (req, res) => res.send(`Hello World! from ${process.env.HOSTNAME`}));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));

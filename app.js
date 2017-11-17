b9e81b7d1374d5846cea42fca01c680cfe0a8c7c
Martin Murphy <martin.murphy@feedhenry.com>
2017-11-17 13:08:53 +0000

    add some logging

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const app = express();
 const port = 4000;
 var shouldFail = false;
 
+const myHost = process.env.HOSTNAME;
+
+function log(str) {
+  const when = (new Date()).toUTCString();
+  console.log(`${when}: ${myHost}: ${str}`);
+}
+
 app.get('/sys/ready', (req, res) => {
   if (shouldFail) {
+    log(`returning NOT ready`);
     res.sendStatus(500);
   } else {
     res.json({
@@ -14,19 +22,21 @@ app.get('/sys/ready', (req, res) => {
   }
 });
 app.get('/sys/fail', (req, res) => {
+  log('setting readiness FAIL mode');
   shouldFail = true;
   res.json({
     'status': 'ok'
   })
 });
 app.get('/sys/fix', (req, res) => {
+  log('setting readiness FIX mode');
   shouldFail = false;
   res.json({
     'status': 'ok'
   })
 });
 app.get('/', (req, res) => {
-  res.send(`Hello World! from ${process.env.HOSTNAME}`);
+  res.send(`Hello World! from ${myHost}`);
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => log(`Example app listening on port ${port}!`));
